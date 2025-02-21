06c5dc6943c9b6e1282f4d171895d747ef6c4faa
CletusReji <cletusreji4191@gmail.com>
2025-02-21 16:29:35 +0530

    created model 2
    created a second model with more epochs (50) and an extra layer than previous model

--- randomtest.py
+++ randomtest.py
@@ -1,8 +1,2 @@
-#import os
 import tensorflow as tf
-
-# Set the environment variable to disable oneDNN custom operations
-#os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-
-print(tf.__version__)
-print(tf.keras.__version__)
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
